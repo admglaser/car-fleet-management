8ab4f726739bd1ea7ee3ee220c2472b5aed8f682
Adam Glaser <adm.glaser@gmail.com>
2021-05-05 11:24:33 +0200

    Add client

---
+++ client/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/cars">Cars</router-link> |
+    </div>
+    <router-view/>
+  </div>
+</template>
