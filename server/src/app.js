8a03ec6acd86f65e8c230f840cfbcc1bd165aff0
Adam Glaser <adm.glaser@gmail.com>
2021-05-08 21:55:49 +0200

    Return with strings

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const { Api, FacebookAuth } = require("./routes");
 
-function App({ carDao, carTypeDao, userDao }) {
+function App({ carDao, carTypeDao, fuelTypeDao, userDao }) {
   const app = express();
   app.use("/", express.static(path.join(__dirname, "../../client/dist/")));
   app.use(FacebookAuth({ app, userDao }));
--- server/src/dao/fuelTypeDao.js
+++ server/src/dao/fuelTypeDao.js
@@ -13,9 +13,7 @@ async function FuelTypeDao({ cloudant }) {
     const { rows } = await cloudant
       .use(TABLE_NAME)
       .list({ include_docs: true });
-    return rows.map((row) => ({
-      name: row.doc._id,
-    }));
+    return rows.map((row) => row.doc._id);
   }
 
   return {
